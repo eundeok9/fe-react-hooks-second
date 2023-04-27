cae84bd2afd1d4a6058da5a43fc59bb61d6203a8
eunseo <eundeok9@gmail.com>
2023-04-27 15:29:22 +0900

    다른 과제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import RandomGame from "./components/RandomGame";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <RandomGame />
       <TodoList />
-    </>
+    </div>
   );
 }
 
