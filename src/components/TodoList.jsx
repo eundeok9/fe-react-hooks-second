fe12273d7540decf910742f11f56fae4cf71e02b
eunseo <eundeok9@gmail.com>
2023-04-28 00:58:31 +0900

    아기사자 과제까지 오나료?

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,32 +1,44 @@
 import {useState, useEffect} from "react";
 
 function TodoList() {
-  const [todos, setTodos] = useState([]); // todo list를 담음
-  const [inputValue, setInputValue] = useState(""); // 입력한 todo를 담음
+  const [todos, setTodos] = useState([]); // todo list를 담는 state
+  const [inputValue, setInputValue] = useState(""); // 입력한 todo를 담는 state
+  const [count, setCount] = useState(0); // 완료한 todo의 개수를 담는 state
 
   useEffect(() => {
     const data = [
+      // 이곳에 프로그램이 처음 렌더링 될 때 표시될 todo list를 {id, text, completed}를 키로 갖는 객체 형식으로 작성해주세요
+      /*********************정답 코드*********************/
       {id: 1, text: "산책 가기", completed: false},
       {id: 2, text: "멋사 과제 하기", completed: true},
       {id: 3, text: "야구 보기", completed: false},
+      /**************************************************/
     ];
+    //이곳에 todos를 변경시키는 코드를 작성해주세요.
     setTodos(data);
   }, []);
 
-  // 입력한 값 저장하는거
+  useEffect(() => {
+    let n = todos.filter((a) => a.completed).length;
+    setCount(n);
+  }, [todos]);
+
+  useEffect(() => {
+    if (count >= 5) alert("오늘 5개나 완료하셨네요!");
+  }, [count]);
+
   const handleInputChange = (e) => {
+    // 이곳에 입력한 todo 내용을 저장하도록 하는 코드를 작성해주세요.
     setInputValue(e.target.value);
   };
 
-  // 추가 버튼 누르면 todos에 새로 입력한 todo가 반영됨.
   const handleFormSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // 추가 버튼 누를 시 자동으로 새로고침 되는 것을 방지
     const newTodo = {id: Date.now(), text: inputValue, completed: false};
     setTodos([...todos, newTodo]);
     setInputValue("");
   };
 
-  // 완료한 todo 를 클릭하면 completed를 true로 변경
   const handleTodo = (id) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
@@ -34,12 +46,14 @@ function TodoList() {
       }
       return todo;
     });
+    // 이곳에 변경된 completed가 todos에 반영되도록 하는 코드를 작성해주세요
     setTodos(updatedTodos);
   };
 
   return (
     <div className="todolist">
       <h1>Todo List</h1>
+      <p>현재 {count}개 완료</p>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
